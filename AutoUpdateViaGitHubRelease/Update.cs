1ceb8edaaca2ae7d4ca201a5d43eec1e6488795a
scherzer <scherzer@hs-weingarten.de>
2019-07-01 13:28:34 +0200

    set Available on calling thread

--- AutoUpdateViaGitHubRelease/Update.cs
+++ AutoUpdateViaGitHubRelease/Update.cs
@@ -24,7 +24,7 @@ namespace AutoUpdateViaGitHubRelease
 					return false;
 				}
 			}
-			Task.Run(DownloadNewVersion).ContinueWith(task => Available = task.Result);
+			Task.Run(DownloadNewVersion).ContinueWith(task => Available = task.Result, TaskContinuationOptions.ExecuteSynchronously);
 		}
 
 		public bool Available
